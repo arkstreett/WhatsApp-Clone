371f78235249c02687f367a04e2a7c67adcf1d53
unknown <arjstreett@gmail.com>
2020-09-19 15:37:47 -0400

    realtime functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import Pusher from "pusher-js"
+import axios from "./axios"
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('messages/sync')
+      .then(response => {
+        console.log(response.data)
+        setMessages(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('9b69f604c6442faa8865', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages}/>
       </div>
     </div>
   );
